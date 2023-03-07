cdb13cda4546b05f2227372e7c60f250247fcb48
Marcos <MarcosCornelius@hotmail.com>
2023-03-06 21:34:11 -0300

    - Department Class - Database configuration

--- src/main/java/br/upf/patrimony/domain/model/Department.java
+++ src/main/java/br/upf/patrimony/domain/model/Department.java
@@ -3,16 +3,16 @@ package br.upf.patrimony.domain.model;
 import jakarta.persistence.*;
 
 /**
- * @Entity - Entidade Departamento
- * @Table - Define o nome da tabela atribuída à classe
+ * "@Entity" - Entidade Departamento
+ * "@Table" - Define o nome da tabela atribuída à classe
  */
 @Entity
 @Table(name = "tb_department")
 public class Department {
     /**
      * ID do departamento
-     * @Id - Define o atributo id como coluna de chave primária da tabela
-     * @GeneratedValue - Gera o valor automaticamente
+     * "@Id" - Define o atributo id como coluna de chave primária da tabela
+     * "@GeneratedValue" - Gera o valor automaticamente
      */
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -20,7 +20,7 @@ public class Department {
 
     /**
      * Nome do departamento
-     * @Column - configura as opções de coluna para o atributo
+     * "@Column" - configura as opções de coluna para o atributo
      */
     @Column(name = "dep_name", length = 60)
     private String name;
