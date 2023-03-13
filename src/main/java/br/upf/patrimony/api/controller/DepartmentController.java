40d65eee4b01c3d9910730063800031d7f017e37
a146676 <146676@LCI-8-13.UPF>
2023-03-13 20:20:00 -0300

    getDepartmentById Database config

--- src/main/java/br/upf/patrimony/api/controller/DepartmentController.java
+++ src/main/java/br/upf/patrimony/api/controller/DepartmentController.java
@@ -4,9 +4,11 @@ import br.upf.patrimony.domain.model.Department;
 import br.upf.patrimony.domain.repository.DepartmentRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/departments")
@@ -26,6 +28,33 @@ public class DepartmentController {
     @ResponseStatus(HttpStatus.OK)
     public List<Department> findAll(){
         return repository.findAll();
+    }
+
+    /**
+     * ResponseEntity<T> Retorna o objeto desejado na forma de resposta HTTP
+     * @param departmentId
+     * @return
+     */
+    @GetMapping("/{departmentId}")
+    public ResponseEntity<?> findById(@PathVariable Long departmentId){
+        //Não trata valores nulos
+        //Department department = repository.findById(departmentId).get();
+
+        /**
+         * Pode retornar valores nulos
+         * Exige validação
+         */
+        Optional<Department> department = repository.findById(departmentId);
+
+        //Verifica se o departamento foi encontrado
+        //Se sim, retorna o objeto encontrado
+        //Se não, retorna erro 404
+        if(department.isPresent()){
+            return ResponseEntity.ok(department.get());
+        } else {
+            //Retorna o código 404 juntamente com a resposta de erro
+            return ResponseEntity.notFound().build();
+        }
     }
 
     /**
---
+++ src/main/resources/data.sql
@@ -0,0 +1,3 @@
+INSERT INTO tb_department (name) VALUES ('Sala de aula');
+INSERT INTO tb_department (name) VALUES ('Sala dos professores');
+INSERT INTO tb_department (name) VALUES ('Secretaria');
